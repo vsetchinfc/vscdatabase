08a03f970e89d4cec3d152a167f878c8bc30203a
Vladislav Setchin <vlad@setchinfc.com.au>
2018-11-29 23:01:05 +1100

    Added removal of temporary db test file. Temporary work around.

--- VSCDatabase/SQLiteInMemoryDatabase.cs
+++ VSCDatabase/SQLiteInMemoryDatabase.cs
@@ -1,15 +1,25 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
+using System.IO;
 
 namespace VSC.Data
 {
     internal class SQLiteInMemoryDatabase : ISCDatabase
     {
+        private const string TestDBName = "slt_test.db";
         public string GeneratorId { get; } = "Sqlite";
 
-        public string ConnectionString { get; private set; } = "Data Source=:memory:;Version=3;New=True;";
+        public string ConnectionString { get; private set; } = string.Format("Data Source={0}", TestDBName); // "Data Source=:memory:;Version=3;New=True;";
         
+        public SQLiteInMemoryDatabase()
+        {
+            if (File.Exists(TestDBName))
+            {
+                File.Delete(TestDBName);
+            }
+        }
+
         public IDbConnection GetDBConnection()
         {
             return new SQLiteConnection(this.ConnectionString);
